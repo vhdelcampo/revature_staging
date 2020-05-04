59fc3079e85431b0f7696f60f20a4388f3d96267
vhdelcampo <vhdelcampo@miners.utep.edu>
2020-05-04 14:07:10 -0500

    add BalanceBracket challenge

---
+++ BalanceBrackets/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BalanceBrackets
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s1 = "((()))()";
+      string s2 = "()())))";
+      string s3 = "(())()())";
+      Console.WriteLine(FixBrackets(s1));
+      Console.WriteLine(FixBrackets(s2));
+      Console.WriteLine(FixBrackets(s3));
+    }
+
+    public static int FixBrackets(string s)
+    {
+      int RightParenCount = 0;
+      int LeftParenCount = 0;
+      int FixCount = 0;
+      for (int i = s.Length - 1; i >= 0; i--)
+      {
+        if (s[i] == ')')
+        {
+          RightParenCount++;
+          FixCount++;
+        }
+        else if (s[i] == '(')
+        {
+          LeftParenCount++;
+          if (RightParenCount > 0)
+          {
+            FixCount--;
+            RightParenCount--;
+          }
+        }
+      }
+      return FixCount;
+    }
+  }
+}
